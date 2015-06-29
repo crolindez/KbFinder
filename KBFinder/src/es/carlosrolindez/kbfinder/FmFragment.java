9da6bb267ccd7067afd66d594d2353cd2eb09bf9
Carlos <Carlos@Carlos-HP>
2015-06-29 23:17:21 +0200

    Fm dial working

--- KBFinder/src/es/carlosrolindez/kbfinder/FmFragment.java
+++ KBFinder/src/es/carlosrolindez/kbfinder/FmFragment.java
@@ -4,8 +4,8 @@ import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,7 +13,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 
@@ -106,40 +105,79 @@ public class FmFragment extends Fragment {
     	RDSText.setText(RDS);    	
     }  
     
-    public void showFmDialog() {
+	public void showFmDialog() {
+    	final int minFreq = 87;
+    	final int maxFreq = 108;
+    	final String dialMegaherz[] = new String[maxFreq-minFreq+1];
+
+    	for (int frInt=maxFreq, index=0; frInt>=minFreq; frInt--,index++){
+    		dialMegaherz[index] = Integer.toString(frInt);
+    	}
+ 
+    	final String dialKiloherz[] = new String[10];
+    	for (int frInt=9, index=0; frInt>=0; frInt--,index++){
+    		dialKiloherz[index] = Integer.toString(frInt);
+    	}
+
+        String freq[] = frequencyText.getText().toString().trim().split("\\.");
+        final String newFreq[] = {freq[0], freq[1]};
+        
+    	final CountDownTimer countDownFm = new CountDownTimer(500,500){
+    		public void onTick(long millisUntilFinished) {
+		    }
+		     	
+		    public void onFinish() {
+		    	spp.sppMessage("TUN "+newFreq[0]+"."+newFreq[1]+"\r",false);
+				if (newFreq[0].length()<3)
+					setFrequency(" "+newFreq[0]+"."+newFreq[1]);
+				else
+					setFrequency(newFreq[0]+"."+newFreq[1]);
+				setRDS("");
+		    }
+    	};
+    	  	
+    	
     	final Dialog fmDialog = new Dialog(mContext, R.style.CustomDialog);
     	fmDialog.setTitle(getResources().getString(R.string.fm_dial));
     	fmDialog.setContentView(R.layout.fm_dial);
         final FmPicker megaherzPicker = (FmPicker) fmDialog.findViewById(R.id.megaherzs);
         final FmPicker kiloherzPicker = (FmPicker) fmDialog.findViewById(R.id.kiloherzs);
- 
-        String freq[] = frequencyText.getText().toString().trim().split(".", 2);
-        Log.e(TAG,frequencyText.getText().toString().trim());
         
-        megaherzPicker.setMaxValue(108);
-        megaherzPicker.setMinValue(87);
+        megaherzPicker.setMaxValue(maxFreq-minFreq);
+        megaherzPicker.setMinValue(0);
         megaherzPicker.setWrapSelectorWheel(true); 
-        Log.e(TAG,"x"+freq[0]);
-        Log.e(TAG,"y"+freq[1]);
-        megaherzPicker.setValue(Integer.parseInt(freq[0]));
+        megaherzPicker.setDisplayedValues(dialMegaherz);
+        megaherzPicker.setValue(maxFreq-Integer.parseInt(freq[0]));
         megaherzPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker numberPicker, int i, int i2) {
-
-                Toast.makeText(mContext, "Value was: " + Integer.toString(i) + " is now: " + Integer.toString(i2), Toast.LENGTH_SHORT).show();
-
+            	newFreq[0] = Integer.toString(maxFreq-i2);
+            	if (newFreq[0].equals("108")) {
+            		newFreq[1] = "0";
+            	} else if ( (newFreq[0].equals("87")) && (Integer.parseInt(newFreq[1])<5) ) {
+            		newFreq[1] = "5";
+            	} else newFreq[1] = Integer.toString(9-kiloherzPicker.getValue());      		
+            	countDownFm.cancel();
+            	countDownFm.start();
             }
         });
         kiloherzPicker.setMaxValue(9);
         kiloherzPicker.setMinValue(0);
         kiloherzPicker.setWrapSelectorWheel(true);
-        megaherzPicker.setValue(Integer.parseInt(freq[1]));
-        megaherzPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+        kiloherzPicker.setDisplayedValues(dialKiloherz);
+        kiloherzPicker.setValue(9-Integer.parseInt(freq[1]));
+        kiloherzPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker numberPicker, int i, int i2) {
-
-                Toast.makeText(mContext, "Value was: " + Integer.toString(i) + " is now: " + Integer.toString(i2), Toast.LENGTH_SHORT).show();
-
+            	newFreq[1] = Integer.toString(9-i2);
+            	if (newFreq[0].equals("108")) {
+            		newFreq[1] = "0";
+            	} else if ( (newFreq[0].equals("87")) && (Integer.parseInt(newFreq[1])<5) ) {
+            		newFreq[1] = "5";
+            	}            		
+            	
+            	countDownFm.cancel();
+            	countDownFm.start();
             }
         });
 
